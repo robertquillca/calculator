ce78c26ee31da186da98f2ea32394ef22927e125
robertquillca <robertquillca@gmail.com>
2022-03-02 19:10:03 -0500

    added stric mode and documentReady funcition

--- js/index.js
+++ js/index.js
@@ -1,36 +1,57 @@
-const display = document.querySelector("#screen");
-const buttons = document.querySelectorAll("button");
+"use strict"
 
-buttons.forEach((item) => {
+const documentReady = () => {
+  const valueElement = document.querySelector("screen-value");
+
+  const display = document.querySelector("#screen");
+  const buttons = document.querySelectorAll("button");
   
-  item.onclick = () => {
-
-    if (item.id == "clear") {
-      display.innerText = "";
-
-    } else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-
-    } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-      
-    } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-
-    } else {
-      display.innerText += item.id;
-    }
+  buttons.forEach((item) => {
+    
+    item.onclick = () => {
+  
+      if (item.id == "clear") {
+        display.innerText = "";
+  
+      } else if (item.id == "backspace") {
+        let string = display.innerText.toString();
+        display.innerText = string.substr(0, string.length - 1);
+  
+      } else if (display.innerText != "" && item.id == "equal") {
+        display.innerText = eval(display.innerText);
+        
+      } else if (display.innerText == "" && item.id == "equal") {
+        display.innerText = "Empty!";
+        setTimeout(() => (display.innerText = ""), 2000);
+  
+      } else {
+        display.innerText += item.id;
+      }
+    };
+  });
+  
+  const themeToggleBtn = document.querySelector(".calculator__theme");
+  const calculator = document.querySelector(".calculator");
+  const toggleIcon = document.querySelector(".calculator__theme-icon");
+  let isDark = true;
+  themeToggleBtn.onclick = () => {
+    calculator.classList.toggle("dark");
+    themeToggleBtn.classList.toggle("active");
+    isDark = !isDark;
   };
-});
+  
 
-const themeToggleBtn = document.querySelector(".calculator__theme");
-const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".calculator__theme-icon");
-let isDark = true;
-themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
